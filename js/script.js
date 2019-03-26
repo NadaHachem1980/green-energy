571196bfd95ac0bd6293d9d8a12667b34f217a99
Nada Hachem <46658614+NadaHachem1980@users.noreply.github.com>
2019-03-25 21:31:19 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,46 +34,27 @@ function setResources(){
 window.addEventListener("load", preloader);
 
     
-/* Get all buttons in a NODE LIST of buttons (array like structure) */
+/* Get all buttons in a LIST of buttons (array like structure) */
 var buttons = [];
 buttons[0] = document.getElementsByName("solar")[0];
 buttons[1] = document.getElementsByName("wind")[0];
 buttons[2] = document.getElementsByName("hydro")[0];
     
-/* Complete your resource-object that will store the dynamic content.
-Resource object should 3 sub-objects. Give your sub-objects
-meaningful names. Every sub-object should have the following
-properties headingContent, bodyText, imgUrl and imgAlt. */
+
 var resource = [];
 
-/* Get the reference to your HTML-container
-that will be dynamically loaded from the resource-object. */
+
 
 var contentHead = document.getElementById("content-head");
 var contentImg = document.getElementById("content-img");
 var contentBody = document.getElementById("content-body");
     
-/* 
-The first button in a NODE LIST of buttons will initially 
-have the id: active-button - this will uniquely style 
-the active button (CSS rule). 
+
     
-The first content from the resource-object will be loaded on the page load:
-`<h1>${headingContent}</h1>
- <img src="${imgUrl}" alt="${imgAlt}">
- <p>${bodyText}</p>` */
-    
-/* Start your handleSelection function here. */ 
+
 function handleSelection(evt){
         
-/* Remove the id active-button from the element that
-contains it prior to the click-event. 
 
-This will require the loop throught the NODE LIST of buttons. 
-Inside the loop, use conditional and the element object method
-hasAttribute() to check if the current button in the loop containes the id.
-If it does, use element-object property removeAttribute()
-to remove the id. */
     for (i = 0; i < buttons.length; i++) { 
       if(buttons[i].hasAttribute("id")){
           buttons[i].removeAttribute("id");
@@ -81,19 +62,9 @@ to remove the id. */
       }
     }
 
-/* Use the element-object method setAttribute() to set the id active-button 
-to the currently clicked button. */
+   this.setAttribute("id", "active-button");
     
-    this.setAttribute("id", "active-button");
-    
-/* Use conditional and event-object to check which button is clicked
-and based on that, create HTML with the data inside the backticks:
-`<h1>${headingContent}</h1>
- <img src="${imgUrl}" alt="${imgAlt}">
- <p>${bodyText}</p>`
-Assign this content to to your HTML-container that will 
-be dynamically loaded (you already got the reference to 
-this container before you started the function handleSelection) */
+
     
     if(buttons[0]===this){
         contentHead.innerHTML = (resource["solar"].headingContent);
@@ -116,13 +87,11 @@ this container before you started the function handleSelection) */
         contentBody.innerHTML = (resource["hydro"].bodyText);
     }
     
-/* Close your handleSelection function here. */  
+ 
 }
 
 
     
-/*  Register all buttons to click event. The event-handler handleSelection will listen 
-for this event to happen. */
 for (i = 0; i < buttons.length; i++) { 
     buttons[i].onclick = handleSelection;
 }
